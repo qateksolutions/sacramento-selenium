ce8ab6ddab8ac89ce4d5773493cbfe51b1609ad8
Qa Tek SOlutions <qateksolutions@gmail.com>
2023-12-03 21:57:17 -0500

    first commit

--- src/main/java/command_providers/ActOn.java
+++ src/main/java/command_providers/ActOn.java
@@ -5,14 +5,17 @@ import org.openqa.selenium.WebDriver;
 
 public class ActOn {
     public static BrowserActions browser(WebDriver driver) {
+
         return new BrowserActions(driver);
     }
 
     public static ElementActions element(WebDriver driver, By locator) {
+
         return new ElementActions(driver, locator);
     }
 
     public static WaitFor wait(WebDriver driver, By locator) {
+
         return new WaitFor(driver, locator);
     }
 }
--- src/test/java/automation_test/mortgage_calculator_parameterized/CalculateRealAprRateParameterized.java
+++ src/test/java/automation_test/mortgage_calculator_parameterized/CalculateRealAprRateParameterized.java
@@ -2,12 +2,10 @@ package automation_test.mortgage_calculator_parameterized;
 
 import automation_test.BaseClass;
 import automation_test.data.DataStore;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import page_objects.NavigationBar;
 
 public class CalculateRealAprRateParameterized extends BaseClass {
-    @BeforeMethod
     @Test(dataProvider = "RealAprRates", dataProviderClass = DataStore.class)
     public void calculateRealApr(String homePrice, String downPayment, String interestRate, String expectedApr) throws Exception {
         new NavigationBar(driver)
